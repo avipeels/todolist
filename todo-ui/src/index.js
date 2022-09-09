69cd6a181955b3ec8e6a4065301d3496ec0e1bdf
avinash peelukhana <avinashpsk@gmail.com>
2022-09-09 19:46:32 +0530

    app  files  moved to App folder

--- todo-ui/src/index.js
+++ todo-ui/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TodoList from './App';
+import TodoList from './ui/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
